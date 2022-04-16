416d44f006c2ae82dc99f4f2bf964becd300a322
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2022-04-16 16:20:20 +0300

    Added interfaces.

---
+++ Store.BLL/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Store.BLL.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    public Task<T?> GetById(int id, out T entity);
+
+    public Task Create(T entity);
+
+    public Task Update(T entity);
+
+    public Task Delete(int id);
+
+    public Task<List<T>> GetRange(int startId, int count);
+}
---
+++ Store.BLL/Interfaces/IService.cs
@@ -0,0 +1,14 @@
+﻿namespace Store.BLL.Interfaces;
+
+public interface IService<T> where T : class
+{
+    public Task<bool> TryGetById(int id, out T entity);
+
+    public Task<T> Create(T entity);
+
+    public Task<T> Update(T entity);
+
+    public Task<bool> Delete(int id);
+
+    public Task<bool> TryGetRange(int startId, int count, out IEnumerable<T> entities);
+}
