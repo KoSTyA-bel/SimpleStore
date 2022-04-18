5a9d01078855a155bea8c0ce669b5b7734a2e310
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2022-04-18 16:17:54 +0300

    Add mapping.

--- Store/MappingProfile.cs
+++ Store/MappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Store.BLL;
 using Store.BLL.Entities;
+using Store.DLL.Entities;
 using Store.Models;
 
 namespace Store;
@@ -11,5 +12,10 @@ public class MappingProfile : Profile
     {
         CreateMap<UserViewModel, User>().ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password.GetMD5Hash()));
         CreateMap<User, UserViewModel>().ForMember(dest => dest.Password, opt => opt.MapFrom(src => string.Empty));
+
+        CreateMap<ProductMongo, Product>().ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.ToString()));
+        CreateMap<Product, ProductMongo>().ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id));
+
+        CreateMap<Product, ProductViewModel>().ReverseMap();
     }
 }
---
+++ Store/Models/ProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Store.Models;
+
+public class ProductViewModel
+{
+    public ProductViewModel()
+    {
+        Id = string.Empty;
+        Name = string.Empty;
+        Description = string.Empty;
+        Created = DateTime.UtcNow;
+        StartOfSales = DateTime.UtcNow;
+    }
+
+    public string Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public int Count { get; set; }
+
+    public DateTime Created { get; init; }
+
+    public DateTime StartOfSales { get; set; }
+}
