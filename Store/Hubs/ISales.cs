e52a0b48edce3c2be813d6f4aa94e0efaab29cd4
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2022-06-13 09:24:50 +0300

    Added documentation to the sales hub.

--- Store/Hubs/ISales.cs
+++ Store/Hubs/ISales.cs
@@ -2,9 +2,22 @@
 
 namespace Store.Hubs;
 
+/// <summary>
+/// Provides methods implemented on the cient side.
+/// </summary>
 public interface ISales
 {
-    public Task StartSales(string product);
+    /// <summary>
+    /// Notifies the client about the start of sales.
+    /// </summary>
+    /// <param name="product">Product id.</param>
+    /// <returns>Task.</returns>
+    public Task StartSales(string productId);
 
+    /// <summary>
+    /// Notifies the client of product data changes.
+    /// </summary>
+    /// <param name="product">Specific product.</param>
+    /// <returns>Task.</returns>
     public Task ProductDataChanged(Product product);
 }
--- Store/Hubs/SalesHub.cs
+++ Store/Hubs/SalesHub.cs
@@ -5,20 +5,38 @@ using Store.DLL.Listeners;
 
 namespace Store.Hubs;
 
+/// <summary>
+/// provides methods to be called by clients.
+/// </summary>
 public class SalesHub : Hub<ISales>
 {
     private readonly IProductDatabaseListener _listener;
 
+    /// <summary>
+    /// Initialize a new instanse of the <see cref="SalesHub"/>.
+    /// </summary>
+    /// <param name="listener">Product database listener.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="listener"/> in null.</exception>
     public SalesHub(ProductDatabaseListener listener)
     {
         _listener = listener ?? throw new ArgumentNullException(nameof(listener));
     }
 
+    /// <summary>
+    /// Adds client to the listening group.
+    /// </summary>
+    /// <param name="productId">Product id.</param>
+    /// <returns>Task.</returns>
     public async Task ListenProduct(string productId)
     {
         await this.Groups.AddToGroupAsync(Context.ConnectionId, productId);
     }
 
+    /// <summary>
+    /// Makes a product purchase by the client.
+    /// </summary>
+    /// <param name="productId">Product id.</param>
+    /// <returns>Task.</returns>
     [Authorize]
     public async Task BuyProduct(string productId)
     {
