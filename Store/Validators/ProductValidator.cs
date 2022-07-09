36dc7b1ca62e8d44dbb18665bff6e630edcbe142
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2022-07-09 11:29:26 +0300

    Added product validator

---
+++ Store/Validators/ProductValidator.cs
@@ -0,0 +1,14 @@
+﻿using Store.BLL.Entities;
+using FluentValidation;
+
+namespace Store.Validators;
+
+public class ProductValidator : AbstractValidator<Product>
+{
+    public ProductValidator()
+    {
+        RuleFor(x => x.Name).Length(0, 100);
+        RuleFor(x => x.Description).Length(0, 1000);
+        RuleFor(x => x.Count).GreaterThanOrEqualTo(0);
+    }
+}
